9e9ebfae64708356eefac53a634450535ae9b0c9
Michael Kim <khmichaelkim@gmail.com>
2024-04-22 08:12:56 -0700

    updated error logging to supress irrelevant warnings

--- src/main/java/com/warpcast/analysis/App.java
+++ src/main/java/com/warpcast/analysis/App.java
@@ -2,10 +2,19 @@ package com.warpcast.analysis;
 
 import com.warpcast.analysis.dao.FetcherDao;
 import com.warpcast.analysis.analysis.AnalysisManager;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class App {
+
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
+
+        // Configured HttpClient to ignore warnings that do not affect functionality. Redirecting JUL to SLF4J
+        SLF4JBridgeHandler.removeHandlersForRootLogger();
+        SLF4JBridgeHandler.install();
 
         AnalysisManager analysisManager = new AnalysisManager();
         FetcherDao fetcherDao = new FetcherDao();
